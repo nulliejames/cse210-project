5495f7426b4a01d22ad6c292c4342550344aac91
nullie.james@github <nullie.james8@gmail.com>
2026-09-03 03:57:23 +0000

    Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Prompt the user for their grade percentage
+        Console.Write("What is your percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letterGrade = "";
+
+        if (percent >= 90   )
+        {
+            letterGrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        {
+            Console.WriteLine($"Your grade is: {letterGrade}");
+        }
+        if (percent >= 70)
+        {
+            Console.WriteLine($"You passed!");
+        }
+        else
+        {
+            Console.WriteLine($"Better luck next time!");
+        }
     }
 }
